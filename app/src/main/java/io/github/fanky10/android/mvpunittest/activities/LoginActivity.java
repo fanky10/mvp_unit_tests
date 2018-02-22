c735e85adf30529bc317699d75994b274e5b4c19
Facundo Scoccia <facundo.scoccia@globant.com>
2018-02-22 12:38:12 -0500

    Add ButterKnife to quickly handle view binding

--- app/src/main/java/io/github/fanky10/android/mvpunittest/activities/LoginActivity.java
+++ app/src/main/java/io/github/fanky10/android/mvpunittest/activities/LoginActivity.java
@@ -1,15 +1,42 @@
 package io.github.fanky10.android.mvpunittest.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.github.fanky10.android.mvpunittest.R;
+import io.github.fanky10.android.mvpunittest.data.repository.UsersRepository;
+import io.github.fanky10.android.mvpunittest.data.repository.impl.UsersRepositoryImpl;
+import io.github.fanky10.android.mvpunittest.mvp.LoginContract;
+import io.github.fanky10.android.mvpunittest.mvp.model.LoginModel;
+import io.github.fanky10.android.mvpunittest.mvp.presenter.LoginPresenter;
+import io.github.fanky10.android.mvpunittest.mvp.view.LoginView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private LoginContract.Presenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        init();
+    }
+
+    private void init() {
+        ButterKnife.bind(this);
+        // TODO: transform this into DI
+        UsersRepository usersRepository = new UsersRepositoryImpl();
+        LoginContract.View view = new LoginView(this);
+        LoginContract.Model model = new LoginModel(usersRepository);
+
+        presenter = new LoginPresenter(model, view);
+        presenter.init();
+    }
+
+    @OnClick(R.id.btn_login)
+    public void btnLoginClicked() {
+        presenter.onLoginPressed();
     }
 }
--- app/src/main/java/io/github/fanky10/android/mvpunittest/mvp/view/LoginView.java
+++ app/src/main/java/io/github/fanky10/android/mvpunittest/mvp/view/LoginView.java
@@ -1,8 +1,32 @@
 package io.github.fanky10.android.mvpunittest.mvp.view;
 
+import android.app.Activity;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.ref.WeakReference;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.github.fanky10.android.mvpunittest.R;
 import io.github.fanky10.android.mvpunittest.mvp.LoginContract;
 
+// TODO: create BaseView to maintain common activity reference / showToast logic
 public class LoginView implements LoginContract.View {
+
+    @BindView(R.id.txt_username) EditText txtUsername;
+    @BindView(R.id.txt_password) TextView txtPassword;
+    @BindView(R.id.btn_login) TextView btnLogin;
+
+    // activity should never be exposed publicly
+    private WeakReference<Activity> activityWeak;
+
+    public LoginView(Activity activity) {
+        ButterKnife.bind(this, activity);
+        this.activityWeak = new WeakReference<Activity>(activity);
+    }
+
     @Override
     public void init() {
 
@@ -10,26 +34,32 @@ public class LoginView implements LoginContract.View {
 
     @Override
     public String getUsername() {
-        return null;
+        return txtUsername.getText().toString();
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return txtPassword.getText().toString();
     }
 
     @Override
     public void showOnAuthenticationErrorMessage() {
-
+        if (activityWeak.get() != null) {
+            Toast.makeText(activityWeak.get(), "Authentication Error", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public void showWelcomeScreen() {
-
+        if (activityWeak.get() != null) {
+            Toast.makeText(activityWeak.get(), "Welcome Screen Here", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public void showForgotPasswordScreen() {
-
+        if (activityWeak.get() != null) {
+            Toast.makeText(activityWeak.get(), "Forgot Password Screen Here", Toast.LENGTH_SHORT).show();
+        }
     }
 }
